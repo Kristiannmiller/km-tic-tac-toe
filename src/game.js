12f6e0a90b267ef530ee4a5ec1154243cb4addc2
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-01 13:29:23 -0600

    Add constructor properties and methods to Game class

---
+++ src/game.js
@@ -0,0 +1,23 @@
+class Game {
+  constructor(player1, player2) {
+    this.player1 = player1
+    this.player2 = player2
+    this.currentBoard = []
+    this.turn = 0
+  }
+  determineBoardStatus() {
+
+  }
+  determineWin() {
+
+  }
+  determineDraw() {
+
+  }
+  saveWinningBoard() {
+
+  }
+  resetBoard() {
+    
+  }
+}
